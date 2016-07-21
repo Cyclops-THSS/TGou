863853dda54bea9cff04630248acf978bf62d634
Li Zhaoyang <lizy14@yeah.net>
2016-07-21 23:08:00 +0800

    models initial

--- TGouSite/TGouApp/models.py
+++ TGouSite/TGouApp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Consumer (models.Model):
+	pass;
+
+class ShopKeeper (models.Model):
+	pass;
+
+class Shop (models.Model):
+	pass;
+
+class Commodity (models.Model):
+	pass;
+
+class OrderItem (models.Model):
+	pass;
+
+class OrderItem (models.Model):
+	pass;
+
+class CartItem (models.Model):
+	pass;
+
+class Cart (models.Model):
+	pass;
+
+class Comment (models.Model):
+	pass;
+
+class Grading (models.Model):
+	pass;
+
+class ShopCategory (models.Model):
+	pass;
+
+class CommodityCategory (models.Model):
+	pass;
