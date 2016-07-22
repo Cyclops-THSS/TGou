c202d85ab9a9398bcb39191b3accc6ec87cf4793
Cyclops <wyh14@mails.tsinghua.edu.cn>
2016-07-22 09:05:15 +0800

    Group support

--- TGouSite/TGouApp/views.py
+++ TGouSite/TGouApp/views.py
@@ -5,9 +5,13 @@ from .models import Consumer, ShopKeeper
 from .forms import *
 from registration.signals import *
 from django.dispatch import receiver
+from django.contrib.auth.models import Group
 
 @receiver(user_registered)
 def save_profile(sender, user, request, **kwargs):
     form = UserRegForm(request.POST)
-    d = ShopKeeper(user=user) if request.POST.get('type', False) == 'on' else Consumer(user=user)
+    isSK = request.POST.get('type', False) == 'on'
+    d = ShopKeeper(user=user) if isSK else Consumer(user=user)
+    g, create = Group.objects.get_or_create(name='ShopKeeper' if isSK else 'Consumer')
+    g.user_set.add(user)
     d.save()
