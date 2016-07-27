f4259e751a7128a425ff7c33b06fceed65eae987
Li Zhaoyang <lizy14@yeah.net>
2016-07-28 00:05:52 +0800

    vCard finished

--- TGouSite/TGouSite/settings.py
+++ TGouSite/TGouSite/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "public"),
+]
 
 # LOGIN_REDIRECT_URL = '/'
 
