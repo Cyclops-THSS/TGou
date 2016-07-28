10d4e9275792b0e929788481c40b71b4ebe90219
Cyclops <wyh14@mails.tsinghua.edu.cn>
2016-07-28 15:05:52 +0800

    Locale support updated~ (controller)

--- TGouSite/TGouSite/settings.py
+++ TGouSite/TGouSite/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'Templates'),
         ],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
